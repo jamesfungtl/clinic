88d4dad54a9768edb019c2cf5f22fb9c6f09ec5b
gfgchhft1781 <gfgchhft1781@gmail.com>
2026-07-26 23:29:15 +0800

    Updates

---
+++ _next/static/L32PSva3IXlYRf8j0vf8i/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ac0ec6cb70313a21.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
